4d31a4a79c4449e4670ae17492d093ffbcfe126f
Luxchar <luccharlopeau@gmail.com>
2022-12-05 09:30:22 +0100

    primitive

---
+++ primitive-declarations.php
@@ -0,0 +1,9 @@
+<?php
+$firstStr = 'Hello World';
+$firstEscapeStr = "Hello \n World";
+$firstBool = true;
+$firstInteger = 5;
+$firstFloat = 5.5;
+$firstNull = null;
+$firstArray = array(2, null, 'Hi', 5.5, false);
+?>
