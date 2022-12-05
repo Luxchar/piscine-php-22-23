3637f3a5082072647a0c34b47112a72c50babebe
Luxchar <luccharlopeau@gmail.com>
2022-12-05 10:40:18 +0100

    for-numbers

--- for-numbers.php
+++ for-numbers.php
@@ -1,10 +1,11 @@
 <?php
-for ($i = 2; $i < 100; $i++) {
+for ($i = 2; $i < 98; $i++) {
     for ($j = 2; $j < $i; $j++) {
-        if ($i % $j == 0) {
+        if ($i % 2 == 0) {
             $isPrime = false;
             break;
-        } elseif ($i == 97) { echo $i;} 
+        } 
+        elseif ($i == 97) { echo $i; break;} 
         else { echo $i . ", "; break; }
     }
 }
