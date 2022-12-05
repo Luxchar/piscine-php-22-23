65cd3df64731b4916815bde09f37fe7edeb17d7a
Luxchar <luccharlopeau@gmail.com>
2022-12-05 10:10:52 +0100

    ricard

--- its-ricard-time.php
+++ its-ricard-time.php
@@ -1,15 +1,3 @@
-<!-- Create a variable named $nbrRicard.
-
-Using the do {...} while () statement, write a script that will increment the number of ricard drunk while respecting the following conditions:
-
-    Before each ricard drunk, display : Come on ricard number + the number of the current ricard
-    At the 3rd ricard, display: I'll have to stop soon!
-    At the 5th ricard, display: I am no longer very fresh ...
-    At the 7th ricard, stop the loop.
-
-Warning ! Don't forget to skip a line at the end of each echo.
- -->
-
 <?php
 $nbrRicard = 0;
 do {
