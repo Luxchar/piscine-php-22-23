c7c01f636ffe457488c97fd4309f1ba714e388ab
Luxchar <luccharlopeau@gmail.com>
2022-12-05 10:12:01 +0100

    ricard

--- its-ricard-time.php
+++ its-ricard-time.php
@@ -2,11 +2,11 @@
 $nbrRicard = 0;
 do {
     $nbrRicard++;
-    echo "Come on ricard number $nbrRicard";
+    echo "Come on ricard number $nbrRicard\n";
     if ($nbrRicard == 3) {
-        echo "I'll have to stop soon!";
+        echo "I'll have to stop soon!\n";
     } elseif ($nbrRicard == 5) {
-        echo "I am no longer very fresh ...";
+        echo "I am no longer very fresh ...\n";
     } elseif ($nbrRicard == 7) {
         break;
     }
