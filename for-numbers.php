2b48a3b2842530d5021b74d6d2ebcde05159e024
Luxchar <luccharlopeau@gmail.com>
2022-12-05 10:31:32 +0100

    for-numbers

--- for-numbers.php
+++ for-numbers.php
@@ -1,13 +1,16 @@
 <?php
 for ($i = 2; $i < 100; $i++) {
-    $isPrime = true;
     for ($j = 2; $j < $i; $j++) {
         if ($i % $j == 0) {
             $isPrime = false;
             break;
+        } elseif ($i == 97) {
+            echo $i;
+        } 
+        else {
+            echo $i . ", ";
+            break;
         }
     }
-    if ($isPrime) {
-        echo $i . ", ";
-    }
 }
+?>
