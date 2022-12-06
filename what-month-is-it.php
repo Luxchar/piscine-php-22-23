a8a99f7020b686b6a09b180c1e3893dbd2672791
Luxchar <luccharlopeau@gmail.com>
2022-12-06 18:34:30 +0100

    month

--- what-month-is-it.php
+++ what-month-is-it.php
@@ -1,9 +1,41 @@
 <?php
 $month = date('n');
-$months = array(1 => 'January',2 => 'February',3 => 'March',4 => 'April',5 => 'May',6 => 'June',7 => 'July',8 => 'August',9 => 'September',10 => 'October',11 => 'November',12 => 'December');
-for ($i = 1; $i <= 12; $i++) {
-    if ($month == $i) {
-        echo "We are in the month of " . $months[$i];
-    }
+switch ($month) {
+    case 1:
+        echo "We are in the month of January";
+        break;
+    case 2:
+        echo "We are in the month of February";
+        break;
+    case 3:
+        echo "We are in the month of March";
+        break;
+    case 4:
+        echo "We are in the month of April";
+        break;
+    case 5:
+        echo "We are in the month of May";
+        break;
+    case 6:
+        echo "We are in the month of June";
+        break;
+    case 7:
+        echo "We are in the month of July";
+        break;
+    case 8:
+        echo "We are in the month of August";
+        break;
+    case 9:
+        echo "We are in the month of September";
+        break;
+    case 10:
+        echo "We are in the month of October";
+        break;
+    case 11:
+        echo "We are in the month of November";
+        break;
+    case 12:
+        echo "We are in the month of December";
+        break;
 }
 ?>
