93c3266027067b2fad59f0d824eec81b37ad0ab3
Luxchar <luccharlopeau@gmail.com>
2022-12-06 20:13:06 +0100

    reverser

---
+++ line-breaker.php
@@ -0,0 +1,22 @@
+
+<?php
+function breakLines(string $string, int $length): string
+{
+    $string = str_replace('', ' ', $string);
+    $array = explode(' ', $string);
+    $newString = '';
+    $count = 0;
+    if (strlen($string) > $length) {
+        return $string;
+    }
+    foreach ($array as $word) {
+        if ($count + strlen($word) > $length) {
+            $newString .= '\n';
+            $count = 0;
+        }
+        $newString .= $word . ' ';
+        $count += strlen($word) + 1;
+    }
+    return trim($newString);
+}
+?>
